93ba5f8f4325f8e48bd1152ba8478c82d3acafa6
satoshi <satoshi.kumano@kii.com>
2017-07-18 22:55:09 +0900

    Added train.py and (run on floyd hub.)

---
+++ train.py
@@ -0,0 +1,36 @@
+import csv
+from scipy.misc import imread
+import numpy as np
+
+lines = []
+with open('/data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+
+for line in lines[1:]:
+    source_path = line[0]
+    filepath = '/data/' + source_path
+    image = imread(filepath)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Flatten())
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('/output/model.h5')
