15a931b50037f8d341ec624b90a3ff2120c10dfb
satoshi <satoshi.kumano@kii.com>
2017-07-20 23:18:48 +0900

    Enable to load saved model.

--- train.py
+++ train.py
@@ -5,10 +5,7 @@ import sklearn
 from sample_generator import generator
 import sys, getopt
 
-inputFile = ''
-outputFile = ''
-
-def train(inputCSVFile, inputDir, outputDir):
+def train(inputCSVFile, inputDir, inputModel, outputDir):
     lines = []
     with open(inputCSVFile) as csvfile:
         reader = csv.reader(csvfile)
@@ -17,22 +14,27 @@ def train(inputCSVFile, inputDir, outputDir):
 
     from scipy import ndimage
 
-    from keras.models import Sequential
+    from keras.models import Sequential, load_model
     from keras.layers import Flatten, Dense, Lambda
     from keras.layers.convolutional import Conv2D, Cropping2D
     from keras.layers.pooling import MaxPooling2D
 
-    model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-    model.add(Cropping2D(cropping=((70,25),(0,0))))
-    model.add(Conv2D(6, (5, 5), activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Conv2D(6, (5, 5), activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dense(1))
+    if inputModel == "":
+        model = Sequential()
+        model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+        model.add(Cropping2D(cropping=((70,25),(0,0))))
+        model.add(Conv2D(6, (5, 5), activation="relu"))
+        model.add(MaxPooling2D())
+        model.add(Conv2D(6, (5, 5), activation="relu"))
+        model.add(MaxPooling2D())
+        model.add(Flatten())
+        model.add(Dense(120))
+        model.add(Dense(84))
+        model.add(Dense(1))
+        model.compile(loss='mse', optimizer='adam')
+    else:
+        print ('load model: ' + inputModel)
+        model = load_model(inputModel)
 
     from sklearn.model_selection import train_test_split
     train_samples, validation_samples = train_test_split(lines[1:], test_size=0.2)
@@ -40,7 +42,6 @@ def train(inputCSVFile, inputDir, outputDir):
     train_generator = generator(train_samples, inputDir, batch_size=32)
     validation_generator = generator(validation_samples, inputDir, batch_size=32)
 
-    model.compile(loss='mse', optimizer='adam')
     history = model.fit_generator(train_generator,\
                 steps_per_epoch = len(train_samples)/32,\
                 validation_data = validation_generator,\
@@ -55,25 +56,32 @@ def train(inputCSVFile, inputDir, outputDir):
     model.save(outputDir + 'model.h5')
 
 def main(argv):
+    helpmsg = 'train.py -i <inputCSV> -m <inputModel> -d <imageDir> -o <outputDir>'
     try:
-        opts, args = getopt.getopt(argv, "hi:d:o:", ["help", "inputCSV=", "imageDir=", "outputDir="])
+        opts, args = getopt.getopt(argv, "hi:m:d:o:", ["help", "inputCSV=", "inputModel=", "imageDir=", "outputDir="])
     except getopt.GetoptError:
-        print ('train.py -i <inputCSV> -d <imageDir> -o <outputDir>')
+        print (helpmsg)
         sys.exit(2)
     for opt, arg in opts:
         if opt in ('-h', '--help'):
-            print ('train.py -i <inputCSV> -d <imageDir> -o <outputDir>')
+            print (helpmsg)
             sys.exit()
         if opt in ('-i', '--inputCSV'):
             print ('inputFile: ' + arg)
             inputFile = arg
+        if opt in ('-m', '--inputModel'):
+            print ('inputModel: ' + arg)
+            if arg != 'none':
+                inputModel = arg
+            else:
+                inputModel = ""
         if opt in ('-d', '--imageDir'):
             print ('imageDir: ' + arg)
             inputDir = arg
         if opt in ('-o', '--outputDir'):
             print ('outputdir: ' + arg)
             outputDir = arg
-    train(inputFile, inputDir, outputDir)
+    train(inputFile, inputDir, inputModel, outputDir)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
