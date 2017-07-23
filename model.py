7fe339c91df84dd652d8904737160580168867a1
satoshi <satoshi.kumano@kii.com>
2017-07-23 20:18:01 +0900

    Changed architecture.
    Le-Net -> Architecture in the Nvidia paper.
    http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf

--- model.py
+++ model.py
@@ -18,20 +18,21 @@ def train(inputCSVFile, inputDir, inputModel, outputDir):
     from keras.layers import Flatten, Dense, Lambda, Dropout
     from keras.layers.convolutional import Conv2D, Cropping2D
     from keras.layers.pooling import MaxPooling2D
+    from keras.backend import tf as ktf
 
     if inputModel == "":
         model = Sequential()
         model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
         model.add(Cropping2D(cropping=((70,25),(0,0))))
-        model.add(Conv2D(6, (5, 5), activation="relu"))
-        model.add(MaxPooling2D())
-        model.add(Conv2D(6, (5, 5), activation="relu"))
-        model.add(MaxPooling2D())
+        model.add(Conv2D(24, (5, 5), strides=(2,2), activation="relu"))
+        model.add(Conv2D(36, (5, 5), strides=(2,2), activation="relu"))
+        model.add(Conv2D(48, (5, 5), strides=(2,2), activation="relu"))
+        model.add(Conv2D(64, (3, 3), activation="relu"))
+        model.add(Conv2D(64, (3, 3), activation="relu"))
         model.add(Flatten())
-        model.add(Dense(120))
-        model.add(Dropout(0.5))
-        model.add(Dense(84))
-        model.add(Dropout(0.5))
+        model.add(Dense(100))
+        model.add(Dense(50))
+        model.add(Dense(10))
         model.add(Dense(1))
         model.compile(loss='mse', optimizer='adam')
     else:
@@ -39,6 +40,7 @@ def train(inputCSVFile, inputDir, inputModel, outputDir):
         model = load_model(inputModel)
 
     from sklearn.model_selection import train_test_split
+    #train_samples, validation_samples = train_test_split(lines[1:], train_size=0.4, test_size=0.1)
     train_samples, validation_samples = train_test_split(lines[1:], test_size=0.2)
 
     train_generator = generator(train_samples, inputDir, batch_size=32)
