a89a101e7b93d08f4f20835e438b21f0e7187bb2
satoshi <satoshi.kumano@kii.com>
2017-07-23 20:20:45 +0900

    Include left and right camera images.

--- sample_generator.py
+++ sample_generator.py
@@ -7,21 +7,23 @@ def generator(samples, inputDir, batch_size=32):
     while 1: # Loop forever so the generator never terminates
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
             images = []
             measurements = []
+            revises = [0.0, 0.2, -0.2]
             for batch_sample in batch_samples:
-                source_path = batch_sample[0]
-                filepath = inputDir + source_path
-                image = imread(filepath)
-                measurement = float(batch_sample[3])
-                images.append(image)
-                measurements.append(measurement)
-                # Augment data : flipped image.
-                flip_image = np.fliplr(image)
-                flip_measurement = measurement * -1.0
-                images.append(flip_image)
-                measurements.append(flip_measurement)
+                for (i, rev) in zip (range(3), revises):
+                    source_path = batch_sample[i]
+                    source_path = source_path.strip()
+                    filepath = inputDir + source_path
+                    image = imread(filepath)
+                    measurement = float(batch_sample[3])+rev
+                    images.append(image)
+                    measurements.append(measurement)
+                    # Augment data : flipped image.
+                    flip_image = np.fliplr(image)
+                    flip_measurement = (measurement+rev) * -1.0
+                    images.append(flip_image)
+                    measurements.append(flip_measurement)
 
             # trim image to only see section with road
             X_train = np.array(images)
